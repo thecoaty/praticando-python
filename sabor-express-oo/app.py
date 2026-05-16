f6e30dd5146a183cca7e18e63f51086fa6d2114a
Kelvin Moraes <xkzin_@hotmail.com>
2026-05-16 11:52:11 -0300

    finalizando curso python: aplicando a orientação a objetos

--- sabor-express-oo/app.py
+++ sabor-express-oo/app.py
@@ -2,7 +2,10 @@ from modelos.restaurante import Restaurante
 
 rest1 = Restaurante("galber", "japonesa")
 rest2 = Restaurante("praça", "Italiana")
-
+rest1.alternar_estado()
+rest1.receber_avaliacao("Gui", 10)
+rest1.receber_avaliacao("Cley", 8)
+rest1.receber_avaliacao("Emy", 5)
 
 
 def main():
--- sabor-express-oo/modelos/restaurante.py
+++ sabor-express-oo/modelos/restaurante.py
@@ -1,9 +1,11 @@
+from modelos.avaliacao import Avaliacao
 class Restaurante:
     restaurantes = []
     def __init__(self, nome, categoria):
         self._nome = nome.title()
         self._categoria = categoria.title()
         self._ativo = False
+        self._avaliacao = []
         Restaurante.restaurantes.append(self)
 
     def __str__(self):
@@ -11,9 +13,9 @@ class Restaurante:
     
     @classmethod
     def listar_restaurantes(cls):
-        print(f"{"Nome do restaurante".ljust(25)} | {"Categoria".ljust(25)} | {"Status"}")
+        print(f"{"Nome do restaurante".ljust(25)} | {"Categoria".ljust(25)} | {"Avaliação".ljust(25)} | {"Status"}")
         for restaurante in cls.restaurantes:
-            print(f"{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.ativo}")
+            print(f"{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {str(restaurante.calcular_avaliacoes).ljust(25)} | {restaurante.ativo}")
     
     @property
     def ativo(self):
@@ -21,3 +23,19 @@ class Restaurante:
     
     def alternar_estado(self):
         self._ativo = not self._ativo
+
+    def receber_avaliacao(self, cliente, nota):
+        if(0 < nota <= 5):
+            avaliacao = Avaliacao(cliente, nota)
+            self._avaliacao.append(avaliacao)
+        
+        
+
+    @property
+    def calcular_avaliacoes(self):
+        if not self._avaliacao:
+            return "Sem avaliações"
+        soma_das_notas = sum(avaliacao._nota for avaliacao in self._avaliacao)
+        quantidade_de_notas = len(self._avaliacao)
+        media = round(soma_das_notas / quantidade_de_notas,1)
+        return media
